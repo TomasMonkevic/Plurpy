7a780c138b285a89276eaf5710b3e1465cab14d0
Tomas Monkevic <tomasmo@wix.com>
2022-12-20 18:45:50 +0200

    Add account tabel creation sql

---
+++ plurpy-backend/src/main/sql/000_create_account_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE account (
+  id UUID PRIMARY KEY,
+  date_created TIMESTAMP NOT NULL,
+  date_updated TIMESTAMP NOT NULL,
+  revision  INT NOT NULL,
+  name  CHAR(25) NOT NULL,
+  password_hash CHAR(60) NOT NULL,
+)
