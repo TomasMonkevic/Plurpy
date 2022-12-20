1a7387a7ccb130eb58e89f79221605320f93786f
Tomas Monkevic <tomasmo@wix.com>
2022-12-20 18:39:55 +0200

    Insert account to db on signup

--- plurpy-backend/src/main/scala/service/AccountsServiceImpl.scala
+++ plurpy-backend/src/main/scala/service/AccountsServiceImpl.scala
@@ -1,9 +1,12 @@
 package org.tomasmo.plurpy
 package service
 
+import utils.TimeConverters.toTimestamp
+import model.{Account => AccountDto, AccountInfo => AccountInfoDto} //TODO maybe rename the entities
 import persistence.AccountsRepositoryImpl
 import v1.account.AccountsService.ZioAccountsService.ZAccountsService
 import v1.account.AccountsService._
+import v1.account.Account.{Account, AccountInfo}
 
 import io.grpc.Status
 import zio.Console.printLine
@@ -12,8 +15,25 @@ import zio.ZIO
 //TODO later don't depend on specific effect type. Use AccountsRepository
 final case class AccountsServiceImpl(accountsRepo: AccountsRepositoryImpl) extends ZAccountsService[Any, Any] {
   override def signup(request: SignupRequest): ZIO[Any, Status, SignupResponse] = {
-    printLine(s"sdsdf request: $request").orDie zipRight
-      ZIO.succeed(SignupResponse(authToken = s"Very secret token for, ${request.getAccount.name}"))
+    //TODO input validation and mapper
+    val foo = for {
+      a <- accountsRepo.insert(AccountInfoDto(
+        name = request.getAccountInfo.getName,
+        passwordHash = request.password //TODO password hashing
+      ))
+    } yield SignupResponse(account = Option(toProto(a)), authToken = "very-legit-auth-token")
+    foo.orElseFail(Status.INTERNAL) //TODO is this it?
+  }
+
+  //TODO move to separate mapper (maybe even automapper)
+  private def toProto(accDto: AccountDto): Account = {
+    new Account(
+      id = Option(accDto.id.toString),
+      dateCreated = Option(toTimestamp(accDto.dateCreated)),
+      dateUpdated = Option(toTimestamp(accDto.dateUpdated)),
+      revision = Option(accDto.revision),
+      information = Option(AccountInfo(name = Option(accDto.accountInfo.name))),
+    )
   }
 
   override def login(request: LoginRequest): ZIO[Any, Status, LoginResponse] = ???
---
+++ plurpy-backend/src/main/scala/utils/TimeConverters.scala
@@ -0,0 +1,28 @@
+package org.tomasmo.plurpy
+package utils
+
+import com.google.protobuf.timestamp.Timestamp
+
+import java.time.ZoneOffset.UTC
+import java.time.temporal.ChronoUnit
+import java.time.{Instant, LocalDateTime}
+
+//TODO rewrite to extension methods
+object TimeConverters {
+  def toTimestamp(instant: Instant): Timestamp = {
+    val truncatedInstant = truncateToMilis(instant)
+    Timestamp(truncatedInstant.getEpochSecond, truncatedInstant.getNano)
+  }
+
+  def toTimestamp(localDateTime: LocalDateTime): Timestamp = {
+    toTimestamp(localDateTime.toInstant(UTC))
+  }
+
+  def toInstant(timestamp: Timestamp): Instant = truncateToMilis(Instant.ofEpochSecond(timestamp.seconds, timestamp.nanos))
+
+  def toLocalDateTime(instant: Instant): LocalDateTime = LocalDateTime.ofInstant(instant, UTC)
+
+  def toLocalDateTime(timestamp: Timestamp): LocalDateTime = toLocalDateTime(toInstant(timestamp))
+
+  private def truncateToMilis(instant: Instant): Instant = instant.truncatedTo(ChronoUnit.MILLIS)
+}
